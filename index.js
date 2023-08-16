5376bee14c5e8b26532d9cda83e4f1547e6592e1
Shermin Sultana <rima64549@gmail.com>
2023-08-17 03:28:51 +0600

    dom

---
+++ index.js
@@ -0,0 +1,21 @@
+function test(){
+   document.getElementById("info").innerText = "Hello";
+   setText ("info , Hello") 
+    
+    
+}
+function test1(){
+    document.getElementById("info1").innerText = "Helloooo";
+
+}
+function test2(){
+    document.getElementById("info2").innerText = "Helloooooo";
+
+}
+
+//common function for all above function::
+
+function setText (id,text){
+    document.getElementById("id").innerText = text1;
+
+}
