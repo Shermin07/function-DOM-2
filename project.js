734578109702006af67f1736cabc7ff98e4b85ef
Shermin Sultana <rima64549@gmail.com>
2023-08-17 19:22:11 +0600

    add

--- project.js
+++ project.js
@@ -1,10 +1,24 @@
+let total = 0;
 function handlerClick(target){
+    const selectedItems = document.getElementById("selected-items");
 const itemName = target.parentNode.childNodes [1].innerText;
 const li = document.createElement("li");
+ 
 li.innerText = itemName;
 
-const selectedItems = document.getElementById("selected-items");
+
 
 selectedItems.appendChild(li);
+
+const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
+
+total = parseInt(total) + parseInt(price);
+
+const totalAmount = document.getElementById("to");
+
+totalAmount.innerText = total;
+
+const discount = document.getElementById("discount");
+
 
 }
